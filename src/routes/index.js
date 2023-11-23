7a4a2c90666f2cc0e843d8063e98c5ce5958bd58
maheshbasnet089 <basnetmanish089@gmail.com>
2023-11-23 17:41:44 +0545

    add singleProduct,deleteProduct

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,11 +1,16 @@
 import {createRouter,createWebHistory} from 'vue-router'
 import Home from '../views/home/Home.vue'
+import SingleProduct from '../views/singleProduct/SingleProduct.vue'
 
 const routes = [
     {
         path : "/",
         component : Home
     },
+    {
+        path : "/singleBlog/:id",
+        component : SingleProduct
+    }
    
 ]
 const router = createRouter({
---
+++ src/views/singleProduct/SingleProduct.vue
@@ -0,0 +1,56 @@
+<template>
+      <div class="container">
+
+<div class="product-card">
+    <img :src="product.image" alt="Product 1">
+    <div class="product-info">
+        <h2>{{ product.name }}</h2>
+        <p>{{ product.description }}</p>
+        <p>Material : {{ product.material }} </p>
+        <button @click="deleteProduct">Delete me</button>
+      
+    </div>
+</div>
+
+
+
+</div>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+    data(){
+        return {
+            product : {}
+        }
+    },
+    methods : {
+        async fetchProduct(){
+            const productId = this.$route.params.id
+        
+            const response =  await axios.get("https://655ca26325b76d9884fdac10.mockapi.io/products/" + productId)
+            this.product = response.data
+        },
+
+        async deleteProduct(){
+           try{
+            const productId = this.$route.params.id
+            const response =  await axios.delete("https://655ca26325b76d9884fdac10.mockapi.io/products/" + productId)
+            this.$router.push("/")
+           }catch(e){
+            alert(e.message)
+           }
+        }
+    },
+    mounted(){
+        this.fetchProduct()
+    }
+}
+</script>
+
+
+<style src="./SingleProduct.css">
+
+</style>
